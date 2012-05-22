08b07a9db86ca51a932f15587131e99dd7c4bf1d
Mark Sellers <msellers@msn.com>
2012-05-22 16:22:14 -0600

    Changed the incorrect field name: CHTEMPID to CCHTEMPID.

---
+++ MoM.Sponsorship.Child.UIModel/ChildDeparture/ChildDepartureGeneralCommentsEditDataFormUIModel.vb
@@ -0,0 +1,11 @@
+Public Class ChildDepartureGeneralCommentsEditDataFormUIModel
+
+    Private Sub ChildDepartureGeneralCommentsEditDataFormUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
+        Dim departureHelper As New DepartureFormsHelper(Me, Common.CRMHelper.FormMode.Edit)
+        'departureHelper.HandleFormLoad(GetRequestContext().AppUserInfo.AppUserName)
+        departureHelper.UserName = GetRequestContext().AppUserInfo.AppUserName
+        departureHelper.InitializeCodeTableVars()
+        departureHelper.SetRequiredFields()
+    End Sub
+
+End Class
