cbedcca8b72eee87549abe11542c3dbee54a6866
Mark Sellers <msellers@msn.com>
2012-06-22 19:34:06 -0600

    Added the @BATCHROWID parameter to the Commit spec so the batch will insert the batchnumber value from the custom batch table. Took out BATCHNUMBER field/parameter as no need to pass it around.

---
+++ MoM.Sponsorship.Child.Catalog/ChildDepartureSpecs/ChildDeparture.SocialImpact.Edit.vb
@@ -0,0 +1,14 @@
+Imports Blackbaud.AppFx.Server
+
+Public NotInheritable Class ChildDepartureEditDataForm
+    Inherits AppCatalog.AppEditDataForm
+
+    Public Overrides Function Load() As Blackbaud.AppFx.Server.AppCatalog.AppEditDataFormLoadResult
+
+    End Function
+
+    Public Overrides Function Save() As Blackbaud.AppFx.Server.AppCatalog.AppEditDataFormSaveResult
+
+    End Function
+
+End Class
