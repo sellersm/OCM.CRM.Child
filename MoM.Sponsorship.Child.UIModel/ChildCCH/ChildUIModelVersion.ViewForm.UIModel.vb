9e13f89e1cad36828e51b71ff644056a020060b3
Cary Mayeda <cmayeda@ba.org>
2012-07-20 03:20:01 -0600

    Standardized names of ChildEligibility and ChildQuota folders Moved ChildEligibility sprocs into a separate folder

--- MoM.Sponsorship.Child.UIModel/ChildCCH/ChildUIModelVersion.ViewForm.UIModel.vb
+++
@@ -1,7 +0,0 @@
-Public Class ChildUIModelVersionViewFormUIModel
-
-    Private Sub ChildUIModelVersionViewFormUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
-        Me.Fields("VERSION").ValueObject = "Version #VersionNumber#"
-    End Sub
-
-End Class
