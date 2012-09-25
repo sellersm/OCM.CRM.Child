fb228dac2498305fc5b9cba4ed2702240d5310ba
Mark Sellers <msellers@msn.com>
2012-09-24 18:25:39 -0600

    Added the USR_CHILDEXTENSION.CCHZIPFILENAME field to Child/CCH queries.

---
+++ MoM.Sponsorship.Child.UIModel/ChildCCH/CCHChildExtensionWhereILiveFormUIModel.vb
@@ -0,0 +1,7 @@
+Public Class CCHChildExtensionWhereILiveFormUIModel
+
+    Private Sub CCHChildExtensionWhereILiveFormUIModel_Loaded(ByVal sender As Object, ByVal e As Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs) Handles Me.Loaded
+
+    End Sub
+
+End Class
