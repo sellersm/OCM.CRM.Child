f5cd4f0139c34d6b70ea1dac96377624d71f8df4
Cary Mayeda <cmayeda@ba.org>
2012-10-17 21:32:58 -0600

    Fixed Photo Import Modified Website.ChildSearch.Query

--- MoM.Sponsorship.Child.Catalog/Photos/ChildPhoto.BatchAdd.Commit.Add.vb
+++ MoM.Sponsorship.Child.Catalog/Photos/ChildPhoto.BatchAdd.Commit.Add.vb
@@ -33,27 +33,57 @@ Public NotInheritable Class ChildPhotoAddBatchCommitAddDataForm
 	End Function
 
 	Private Function SaveDataDB(ByVal SponsorshipOpportunityLookupID As String, ByVal AttachmentTypeCodeID As Guid, ByVal PictureTitle As String, ByVal PictureFile As String, ByVal FileName As String)
-		Using conn As SqlClient.SqlConnection = Me.RequestContext.OpenAppDBConnection()
-			Dim cmd As SqlClient.SqlCommand = New SqlClient.SqlCommand()
-			cmd.Connection = conn
+		Dim username As String = ""
+		Dim password As String = ""
+		Using Connection As SqlClient.SqlConnection = Me.RequestContext.OpenAppDBConnection()
+			Dim command As SqlClient.SqlCommand = Connection.CreateCommand()
+			With command
 
-			cmd.CommandText = "dbo.USR_USP_CHILDPHOTOADD"
-			cmd.CommandType = CommandType.StoredProcedure
+				'.CommandText = "dbo.USR_USP_IMPORT_GETIMPORTSOURCE"
+				'.CommandType = CommandType.StoredProcedure
 
-			cmd.Parameters.AddWithValue("@VALIDATEONLY", VALIDATEONLY)
-			cmd.Parameters.AddWithValue("@SPONSORSHIPOPPORTUNITYLOOKUPID", SponsorshipOpportunityLookupID)
-			cmd.Parameters.AddWithValue("@ATTACHMENTTYPECODEID", AttachmentTypeCodeID)
-			cmd.Parameters.AddWithValue("@PICTURETITLE", PictureTitle)
-			cmd.Parameters.AddWithValue("@FILENAME", PictureFile)
-			cmd.Parameters.AddWithValue("@PICTURE", GetImage(PictureFile))
+				'.Parameters.AddWithValue("@userName", username)
+				'.Parameters("@userName").Direction = ParameterDirection.Output
+				'.Parameters.AddWithValue("@password", password)
+				'.Parameters("@password").Direction = ParameterDirection.Output
 
-			cmd.ExecuteNonQuery()
+				'.ExecuteNonQuery()
+
+
+				'.Parameters.Clear()
+				.CommandType = CommandType.StoredProcedure
+
+				.CommandText = "dbo.USR_USP_CHILDPHOTOADD"
+				.CommandType = CommandType.StoredProcedure
+
+				.Parameters.AddWithValue("@VALIDATEONLY", VALIDATEONLY)
+				.Parameters.AddWithValue("@SPONSORSHIPOPPORTUNITYLOOKUPID", SponsorshipOpportunityLookupID)
+				.Parameters.AddWithValue("@ATTACHMENTTYPECODEID", AttachmentTypeCodeID)
+				.Parameters.AddWithValue("@PICTURETITLE", PictureTitle)
+				.Parameters.AddWithValue("@FILENAME", PictureFile)
+				.Parameters.AddWithValue("@PICTURE", GetImage(PictureFile))
+
+				.ExecuteNonQuery()
+
+
+			End With
 		End Using
 
+		
 		Return 0
 	End Function
 
+
 	Public Shared Function GetImage(ByVal filePath As String) As Byte()
+
+		Dim impersonationScope As UserImpersonationScope = Nothing
+		Try
+			impersonationScope = ImpersonationHelper.GetImpersonationScope("blackbaudhost\cmayeda21195p", "T#pPE2ki@P")
+		Catch ex As Exception
+			impersonationScope = ImpersonationHelper.GetImpersonationScope("blackbaudhost\caryma21195D", "T#pPE2ki@P")
+
+		End Try
+		
 		Dim stream As FileStream = New FileStream( _
 		   filePath, FileMode.Open, FileAccess.Read)
 		Dim reader As BinaryReader = New BinaryReader(stream)
@@ -62,6 +92,11 @@ Public NotInheritable Class ChildPhotoAddBatchCommitAddDataForm
 
 		reader.Close()
 		stream.Close()
+
+		If impersonationScope IsNot Nothing Then
+			impersonationScope.Dispose()
+		End If
+
 
 		Return imageByte
 	End Function
---
+++ MoM.Sponsorship.Child.Catalog/Photos/ImpersonationHelper.vb
@@ -0,0 +1,83 @@
+﻿Imports Blackbaud.AppFx.Server
+Imports System.IO
+Imports System.Data.SqlClient
+Imports Blackbaud.AppFx.SpWrap
+
+Friend NotInheritable Class ImpersonationHelper
+
+    Friend Shared Function GetImpersonationScope(ByVal usernameAndDomain As String, ByVal password As String) As UserImpersonationScope
+        Dim domain = String.Empty
+        Dim userName = String.Empty
+
+        ParseDomainAndUserName(usernameAndDomain, domain, userName)
+
+        Return New UserImpersonationScope(userName, domain, password)
+    End Function
+
+    Private Shared Sub ParseDomainAndUserName(ByVal domainAndUserName As String, ByRef domain As String, ByRef userName As String)
+        Dim indexOfSlash = domainAndUserName.IndexOf("\", StringComparison.CurrentCulture)
+
+        If indexOfSlash > 0 Then
+            domain = domainAndUserName.Substring(0, indexOfSlash)
+            userName = domainAndUserName.Remove(0, domain.Length + 1)
+        Else
+            domain = String.Empty
+            userName = domainAndUserName
+        End If
+    End Sub
+
+    Friend Shared Sub GrantRightsToFileForImportUser(ByVal impersonateUserName As String, ByVal filePath As String)
+
+        Dim domain As String = Nothing
+        Dim userName As String = Nothing
+
+        ParseDomainAndUserName(impersonateUserName, domain, userName)
+
+        Dim importUserAccount As New Security.Principal.NTAccount(domain, userName)
+
+        'TWG 06/02/2009 Because File.Move is not being used instead of copy, import user must have read, write, delete privledges
+        Dim rule As New Security.AccessControl.FileSystemAccessRule(importUserAccount, Security.AccessControl.FileSystemRights.Read Or Security.AccessControl.FileSystemRights.Write Or Security.AccessControl.FileSystemRights.Delete, Security.AccessControl.AccessControlType.Allow)
+
+        Dim fileSecurity As Security.AccessControl.FileSecurity = File.GetAccessControl(filePath)
+        fileSecurity.AddAccessRule(rule)
+
+        File.SetAccessControl(filePath, fileSecurity)
+
+    End Sub
+
+    Friend Shared Function GetOptions(ByVal conn As SqlConnection) As USP_IMPORTPROCESSOPTIONS_GET.ResultRow
+
+        Return USP_IMPORTPROCESSOPTIONS_GET.ExecuteRow(conn)
+
+    End Function
+
+    Friend Shared Function GetImportReadFileStream(ByVal fileName As String, ByVal impersonate As Boolean, ByVal userNameDomain As String, ByVal password As String) As Stream
+        Dim impersonationScope As UserImpersonationScope = Nothing
+
+        If impersonate Then
+            impersonationScope = ImpersonationHelper.GetImpersonationScope(userNameDomain, password)
+        End If
+        Try
+            Return New FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read)
+        Finally
+            If impersonationScope IsNot Nothing Then
+                impersonationScope.Dispose()
+            End If
+        End Try
+    End Function
+
+    Friend Shared Function GetImportWriteFileStream(ByVal fileName As String, ByVal impersonate As Boolean, ByVal userNameDomain As String, ByVal password As String) As Stream
+        Dim impersonationScope As UserImpersonationScope = Nothing
+
+        If impersonate Then
+            impersonationScope = ImpersonationHelper.GetImpersonationScope(userNameDomain, password)
+        End If
+        Try
+            Return New FileStream(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)
+        Finally
+            If impersonationScope IsNot Nothing Then
+                impersonationScope.Dispose()
+            End If
+        End Try
+    End Function
+End Class
